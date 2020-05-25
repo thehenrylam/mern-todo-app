c5aa23a733728f7fc2da23d2b0ca124f4669f51e
Echo-Gambino <nkb012pomm@gmail.com>
2020-05-25 13:38:29 -0600

    Introduced some css and javascript extensions into frontend/public/index.html and integrated all the new additions in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Navbar from "./components/navbar.component";
 import CreateTodo from "./components/create-todo.component";
 import EditTodo from "./components/edit-todo.component";
 import TodosList from "./components/todos-list.component";
+import Landing from "./components/landing.component";
+import Register from "./components/register.component";
+import Login from "./components/login.component";
 
 import logo from "./logo.png";
 
@@ -12,28 +17,16 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div className="container">
-          
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="https://duckduckgo.com/">
-              <img src={logo} width="30" height="30" alt="Custom logo that I made" />
-            </a>
-            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
-            <div className="nav-collapse">
-              <ul className="navbar-nav mr-auto">
-                <li className="navbar-item">
-                  <Link to="/" className="nav-link">Todos</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/create" className="nav-link">Create Todo</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-
-          <Route path="/" exact component={TodosList} />
-          <Route path="/edit/:id" component={EditTodo} />
-          <Route path="/create" component={CreateTodo} />
+        <div className="App">
+          <Navbar />
+          <div className="container" style={{marginTop: 20}}>
+            <Route path="/" exact component={TodosList} />
+            <Route path="/edit/:id" component={EditTodo} />
+            <Route path="/create" component={CreateTodo} />
+            <Route path="/landing" component={Landing} />
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+          </div>
         </div>
       </Router>
     );
